9ac526475ace989bf2ae76f8fed63aeba571acc0
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-06-18 00:20:21 +0530

    day-4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion,AnimatePresence } from 'framer-motion'
 
 const Home = () => {
   return (
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { proxy } from "valtio";
+
+const state = proxy({
+
+});
+
+export default state;
