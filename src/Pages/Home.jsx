4dd20422d40ac613b2b236ecde334cff8153a308
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-06-18 00:57:18 +0530

    day-4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
           <motion.div className='home-content' {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className='head-text'>
-                LET'S <br className='xl:block hidden'/>
+                LET'S <br className='xl:block hidden'/> DO IT.
               </h1>
             </motion.div>
           </motion.div>
