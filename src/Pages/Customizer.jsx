bef33ab573033d0749dd8162e0df5b5a57be9056
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-07-16 12:49:30 +0530

    day-8

--- src/Pages/Customizer.jsx
+++ src/Pages/Customizer.jsx
@@ -1,14 +1,20 @@
 import { AnimatePresence } from 'framer-motion'
 import React from 'react'
+import { useSnapshot } from 'valtio'
 import state from '../store'
+import { motion } from 'framer-motion'
 
 const Customizer = () => {
-  const snap = useSnapeshot(state);
+  const snap = useSnapshot(state);
 
   return (
     <AnimatePresence>
       {snap.intro && (
-        
+        <>
+        <motion.div>
+          
+        </motion.div>
+        </>
       )}
     </AnimatePresence>
   )
