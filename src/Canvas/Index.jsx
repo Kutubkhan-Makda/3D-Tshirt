f863d51f7a7e7c3fdfb2947e551cf7c3180fb942
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-10-28 12:50:08 +0530

    day-11

--- src/Canvas/Index.jsx
+++ src/Canvas/Index.jsx
@@ -1,8 +1,22 @@
+import { Center, Environment } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
+import Shirt from './Shirt'
 
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+      <CameraRig>
+        <Backdrop/>
+        <Center>
+          <Shirt/>
+        </Center>
+      </CameraRig>
+    </Canvas>
   )
 }
 
