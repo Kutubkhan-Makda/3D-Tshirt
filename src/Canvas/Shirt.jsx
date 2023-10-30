b809111e206b8e784a650e15b8eff86b729f28ee
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-10-30 22:25:55 +0530

    day-12

--- src/Canvas/Shirt.jsx
+++ src/Canvas/Shirt.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+import { useGLTF } from '@react-three/drei';
 
 const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes,materials } = useGLTF('/shirt_baked.glb');
+
   return (
     <div>Shirt</div>
   )
